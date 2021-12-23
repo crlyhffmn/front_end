ace05a6df224d9b05033b23fe11a7840309053b4
jpark143 <jaeypark010@gmail.com>
2021-12-23 17:57:58 -0500

    Update on the Login Form

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
+import {Alert} from 'react-bootstrap';
+import './LoginForm.css';
+
 
 const LoginForm = () => {
     const [users, setUsers] = useState({
@@ -16,27 +19,51 @@ const LoginForm = () => {
 
     function onSubmitHandler(e) {
         e.preventDefault();
+        axios.get("http://localhost:10001/user", users.username)
+        .then(response => {
+            console.log(response.data);
+            if(response.data === ""){
+                return(
+                    <Alert>Please enter your username</Alert>
+                )
+            }else if(response.data.password !== users.password){
+                return(
+                    <Alert>Invalid Password</Alert>
+                )
+            };
+        })
+        .catch(error => {console.error(error)})
+    }
 
+    function onClickHandler(e){
+        e.preventDefault();
+        window.location.href='/Register';
     }
 
     return(
-        <div className="LoginContainer">
-            <div className="wrapper">
-                <h1>Log In</h1>
-                <form onSubmit={onSubmitHandler}>
-                    <div className="mb-3">
-                        <label className="form-label">Username</label>
-                        <input type="text" className="form-control" name="username" placeholder="Username" value={users.username} onChange={onChangeHandler} required/>
+        <div className="container">
+            <div className="containerLeft">
+                <div className="text-left">
+                    <h1>MiniMint</h1>
+                    <h2>Some Mints a day<br/>
+                        Helps keep the bored away.</h2>
+                </div>
+            </div>
+
+            <div className="containerRight">
+                <form className="form">
+                    <div className="form-group">
+                        <input type="text" placeholder="Enter Your Email or Username" name="username" value={users.username} onChange={onChangeHandler} /> <br></br>
+                         <input type="password"  placeholder="Password" name="password" value={users.password} onChange={onChangeHandler} /><br/><br/>
+                        <button onSubmit={onSubmitHandler}>Log In</button>
                     </div>
-                    <div className="mb-3">
-                        <label className="form-label">Password</label>
-                        <input type="password" className="form-control" name="password" placeholder="Password" value={users.password} onChange={onChangeHandler} required/>
+                        {/* <Link to="/update">forgot you password?</Link> */}
+                    <p><a href ="/">forgot your password?</a></p>
+                    <hr/>
+                    <div className="btn">
+                        <button onClick={onClickHandler}>Create an Account</button>
                     </div>
-                    <button type="submit" className="btn btn-primary">Log In</button>
-                    <p>Forgot your password?</p>
-                    <button type="submit" className="btn btn-primary">Create Account</button>
-                </form>
-                
+                </form>                
             </div>
         </div>
     )
