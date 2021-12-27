cd13bac424a5f9ac93d61586a79e6684e7d029b4
LucasUzoka <92759285+LucasUzoka@users.noreply.github.com>
2021-12-27 12:06:27 -0500

    Login page set, form complete, still need to know which page the login button will be routed to

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
+import './login.css';
+import logo from './Logo3.png'
+import { Link } from "react-router-dom";
+import Register from "../Register/Register";
+import { Form, Button, Card, Row, Col, FormGroup } from 'react-bootstrap';
 
 const LoginForm = () => {
     const [users, setUsers] = useState({
@@ -12,34 +17,59 @@ const LoginForm = () => {
             ...users,
             [e.target.name]: e.target.value
         });
-    };
+    }
 
     function onSubmitHandler(e) {
         e.preventDefault();
 
     }
 
+    console.log(logo);
+
     return(
-        <div className="LoginContainer">
-            <div className="wrapper">
-                <h1>Log In</h1>
+
+        <div className='row'>
+            <div className='column'>
+
+                <div className="textbox">
+                    <h1>
+                        MiniMint
+                    </h1>
+                    <h2>
+                        Some Mints a day<br></br>
+                        Helps keep the bored away.
+                    </h2>
+                </div>
+
+            </div>
+
+            <div className="column">
+
                 <form onSubmit={onSubmitHandler}>
+                    <img className='logo' src={logo} alt="mint"></img>
                     <div className="mb-3">
                         <label className="form-label">Username</label>
-                        <input type="text" className="form-control" name="username" placeholder="Username" value={users.username} onChange={onChangeHandler} required/>
+                        <input type="text" className="form-control" name="username" placeholder="Username" value={users.username} onChange={onChangeHandler} required />
                     </div>
                     <div className="mb-3">
                         <label className="form-label">Password</label>
-                        <input type="password" className="form-control" name="password" placeholder="Password" value={users.password} onChange={onChangeHandler} required/>
+                        <input type="password" className="form-control" name="password" placeholder="Password" value={users.password} onChange={onChangeHandler} required />
                     </div>
-                    <button type="submit" className="btn btn-primary">Log In</button>
+                    <Button href="/" type="submit" className="btn-login" size="lg">Log In</Button>
+                    <hr></hr>
                     <p>Forgot your password?</p>
-                    <button type="submit" className="btn btn-primary">Create Account</button>
+                   
+                  
+                    <Button href="/register" type="submit" className="btn-register" size="lg" >Create Account</Button>
+                  
+                   
                 </form>
-                
+
             </div>
         </div>
     )
+
+      
 
 }
 export default LoginForm;
