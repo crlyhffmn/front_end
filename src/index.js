e969c1d2b9f22864ab53a2005d7b656aa94f27ee
Ian Mitchell <ian.mitchell@revature.net>
2021-12-28 11:01:31 -0700

    Fix files after merge conflicts.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import 'react-bootstrap';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
-
-=======
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import {configureStore} from '@reduxjs/toolkit';
->>>>>>> master
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import {userStore} from './userStore'
 
