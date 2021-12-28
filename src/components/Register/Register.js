068eb770d97878687ac44991747da1e4de582d0c
Christian Lombardo <christianlombardo@verizon.net>
2021-12-28 12:23:04 -0500

    Registration duplicate user handling and display message.

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Form, Button, Card, Row, Col, Alert } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { login } from '../../userStore';
 
 const Register = () => {
 
@@ -18,10 +20,11 @@ const Register = () => {
         userPassword: ''
     })
 
+    const dispatch = useDispatch();
+
 
     // states for checking the errors
     const [submitted, setSubmitted] = useState(false);
-    const [error, setError] = useState(false);
     const [errorMsg, setErrorMsg] = useState('');
 
     function onChangeHandler(event) {
@@ -40,39 +43,28 @@ const Register = () => {
 
     // Handle form subsmission
     const checkFieldsHandler = (e) => {
-        console.log("user.username = " + user.username + ", user.email = " + user.userEmail + ", user.password = " + user.password)
+        //console.log("user.username = " + user.username + ", user.userEmail = " + user.userEmail + ", user.userPassword = " + user.userPassword)
 
         // check if any fields are empty
-        if (user.username === "" || user.userEmail === "" || user.password === "") {
+        if (user.username === "" || user.userEmail === "" || user.userPassword === "") {
             console.log("setError(true)")
             setSubmitted(false)
-            setError(true)
-        } else { // everything is good now submit to server
+            setErrorMsg('Please fill out all the fields.')
+        }        
+        else { // everything is good now submit to server
             console.log("setSubmitted(true) setError(false)")
-            setSubmitted(true)
-            setError(false)
+            setErrorMsg('')
         }
     }
 
 
     const displayMessage = () => {
-        if (error) {  // show error message if error is true
+        if (errorMsg !== '') {
             return (
                 <Alert
                     variant='danger'
                     style={{
-                        display: error ? "" : "none",
-                    }}
-                >
-                    Please fill in all fields
-                </Alert>
-            )
-        } else if (errorMsg !== '') {
-            return (
-                <Alert
-                    variant='danger'
-                    style={{
-                        display: error ? "" : "none",
+                        display: errorMsg ? "" : "none",
                     }}
                 >
                     {errorMsg}
@@ -96,44 +88,39 @@ const Register = () => {
 
     const registerHandler = (e) => {
         e.preventDefault();
-
+        dispatch(login(user));
         const form = e.currentTarget;
         if (form.checkValidity() === true) {
             e.preventDefault();
             e.stopPropagation();
-            setValidated(true);
-        // if (error === false) {
             axios.post("http://localhost:10001/user", user)
                 .then(response => {
                     console.log(response.data);
                     clearState();
+                    setSubmitted(true)
                 })
                 .catch(error => {
                     console.error(error);
-                    setErrorMsg(error);
-                    // set duplicate user
-                    // error.response.data.message
-                    console.log(error.response.data.message)
+                    //setErrorMsg(error.response.data.message);
+                    setErrorMsg("User email has already been registered.")
+                    console.log("error.response.data.message = " + error.response.data.message)
                 })
         }
         else {
             setSubmitted(false)
         }
 
-        // const form = e.currentTarget;
-        // if (form.checkValidity() === false) {
-        //     e.preventDefault();
-        //     e.stopPropagation();
-        // }
-        // setValidated(true);
+        setValidated(true);
 
     };
+
+    console.log("errorMsg = " + errorMsg);
 
     return (
         <Card>
             <Card.Title>Registration Form</Card.Title>
             <Card.Body style={{ textAlign: "left" }}>
-                <Form noValidate validated={validated} onSubmit={registerHandler} onError={error => setError(true)}>
+                <Form noValidate validated={validated} onSubmit={registerHandler} >
                     <Form.Group className="mb-3">
                         <Form.Label>Username </Form.Label>
                         <Form.Control
