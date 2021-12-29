6c5ea4e583f49d474e7ea963e8d8f2676429b64d
jpark143 <jaeypark010@gmail.com>
2021-12-29 11:16:52 -0500

    Update Login with store and slice

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -3,28 +3,30 @@ import axios from "axios";
 import {Alert} from 'react-bootstrap';
 import './LoginForm.css';
 import logo from '../../image/Logo3.png';
+import { useDispatch } from "react-redux";
+import { login } from "../../userSlice";
 
 const LoginForm = () => {
-    const [users, setUsers] = useState({
+    const dispatch = useDispatch();
+
+    const [user, setUser] = useState({
         username: '',
         password: ''
     });
 
     function onChangeHandler(e){
-        setUsers({
-            ...users,
+        setUser({
+            ...user,
             [e.target.name]: e.target.value
         });
     };
 
     function onSubmitHandler(e) {
         e.preventDefault();
-        axios.get("http://localhost:10001/user", users)
+        axios.get("http://localhost:10001/user", user)
         .then(response => {
             console.log(response.data);
-            // save it to the store
-
-            // after save, forward to the homepage
+            dispatch(login(user))
         })
         .catch(error => {
             console.error(error)
@@ -50,15 +52,15 @@ const LoginForm = () => {
             </div>
 
             <div className="containerRight">
-                <form className="form">
+                <form className="form" onSubmit={onSubmitHandler}>
                     <img className="logo" src={logo}/>
                     <div className="form-group">
                         <label className="form-label">Username</label>
-                        <input type="text" placeholder="Enter Your Username" name="username" value={users.username} onChange={onChangeHandler} required/> <br></br>
+                        <input type="text" placeholder="Enter Your Username" name="username" value={user.username} onChange={onChangeHandler} required/> <br></br>
                     </div><br/>
                     <div className="form-group">
                         <label className="form-label">Password</label>
-                        <input type="password"  placeholder="Password" name="password" value={users.password} onChange={onChangeHandler} required /><br/><br/>
+                        <input type="password"  placeholder="Password" name="password" value={user.password} onChange={onChangeHandler} required /><br/><br/>
                         <button className="btn-login" onSubmit={onSubmitHandler}>Log In</button>
                     </div>
                     <hr/>
--- src/index.js
+++ src/index.js
@@ -5,26 +5,13 @@ import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {configureStore} from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-import userReducer from './userStore';
-
-
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
-
+import { userStore } from './userStore';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store ={store}>
-    <App />
+    <Provider store ={userStore}>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
